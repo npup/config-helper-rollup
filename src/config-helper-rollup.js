abb53d1179e7807c38887eb8c3eaab04e976920d
P. Envall <petter.envall@gmail.com>
2020-08-25 11:16:21 +0200

    bleh

--- src/config-helper-rollup.js
+++ src/config-helper-rollup.js
@@ -1,210 +1,206 @@
-import path from "path";
+export default ({
+    path,
+    sveltePlugin, resolve, commonjs,
+    autoPreprocess,
+    htmlTemplatePlugin, liveReloadPlugin, stylesPlugin,
+    serve,
+    terser,
+    defaults: _defaults,
+}) => {
 
-import sveltePlugin from "rollup-plugin-svelte";
-import autoPreprocess from "svelte-preprocess";
+    const {
+        defaultDevserverOptions,
+        defaultHtmlTemplateOptions,
+        defaultOptions,
+        defaultStylesOptions,
+        defaultSvelteOptions
+    } = _defaults;
 
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+    return (options = {}) => {
+        options = { ...defaultOptions, ...options };
 
-import serve from "rollup-plugin-serve";
-
-import htmlTemplatePlugin from "rollup-plugin-generate-html-template";
-import liveReloadPlugin from "rollup-plugin-livereload";
-import { terser } from "rollup-plugin-terser";
-
-import stylesPlugin from "rollup-plugin-styles";
-
-import {
-    defaultDevserverOptions,
-    defaultHtmlTemplateOptions,
-    defaultOptions,
-    defaultStylesOptions,
-    defaultSvelteOptions
-} from "./defaults";
-
-export const chr = (options = {}) => {
-    options = { ...defaultOptions, ...options };
-
-    let { production: productionEnvProperty } = options;
-    let isProduction = !!productionEnvProperty;
-    if ("string" == typeof productionEnvProperty) {
-        isProduction = "development" != process.env[productionEnvProperty];
-    }
-
-    let {
-        // basics
-        name,           // mandatory and must be unique
-        src, dist,      // input/output directories
-        entry,          // entry point (file name) for javascript
-
-        sourcemap,      // generate js source maps?
-
-        htmlTemplate,   // use html template? boolean or settings
-        devServer,      // start a development server? boolean or settings
-        svelte,         // handle svelte compilation? boolean or settings
-        styles,         // handle styles compilation? boolean or settings
-
-        minimize: globalMinimize, // crunch file output? boolean or...
-
-    } = options;
-
-    //  minimize: use explicit setting or heed isProduction flag
-    if ("boolean" != typeof globalMinimize) {
-        globalMinimize = isProduction;
-    }
-
-    const info = {
-        "App name": name,
-        "Production mode": isProduction,
-        "Dist": dist,
-        "Generate source maps:": sourcemap,
-        "Minimize (main prop)": globalMinimize,
-    };
-
-
-    if (styles) {
-        if ("boolean" == typeof styles) {
-            styles = {};
+        let { production: productionEnvProperty } = options;
+        let isProduction = !!productionEnvProperty;
+        if ("string" == typeof productionEnvProperty) {
+            isProduction = "development" != process.env[productionEnvProperty];
         }
 
-        styles = options.styles = { ...defaultStylesOptions, ...styles };
+        let {
+            // basics
+            name,           // mandatory and must be unique
+            src, dist,      // input/output directories
+            entry,          // entry point (file name) for javascript
 
-        // transform the "extract" prop to this api
-        const { extract } = styles;
-        const mode = extract ?
-            [ "extract" ] :
-            [ "inject" ];
-        delete styles.extract;
+            sourcemap,      // generate js source maps?
 
-        // minimize as told to, OR do as top prop says
-        let { minimize } = styles;
-        if ("boolean" != typeof styles.minimize) {
-            minimize = globalMinimize;
+            htmlTemplate,   // use html template? boolean or settings
+            devServer,      // start a development server? boolean or settings
+            svelte,         // handle svelte compilation? boolean or settings
+            styles,         // handle styles compilation? boolean or settings
+
+            minimize: globalMinimize, // crunch file output? boolean or...
+
+        } = options;
+
+        //  minimize: use explicit setting or heed isProduction flag
+        if ("boolean" != typeof globalMinimize) {
+            globalMinimize = isProduction;
         }
-        styles = options.styles = {
-            ...styles,
-            mode,
-            minimize,
+
+        const info = {
+            "App name": name,
+            "Production mode": isProduction,
+            "Dist": dist,
+            "Generate source maps:": sourcemap,
+            "Minimize (main prop)": globalMinimize,
         };
-    }
 
-    if (htmlTemplate) {
-        if ("boolean" == typeof htmlTemplate) {
-            htmlTemplate = {};
-        }
-        htmlTemplate = options.htmlTemplate = { ...defaultHtmlTemplateOptions, ...htmlTemplate, };
-    }
-    if (devServer) {
-        if ("boolean" == typeof devServer) {
-            devServer = {};
-        }
-        devServer = options.devServer = { ...defaultDevserverOptions, ...devServer, };
-        info["Port"] = devServer.port;
-        info["Use livereload"] = devServer.livereload;
-    }
 
-    if (svelte) {
-        if ("boolean" == typeof svelte) {
-            svelte = { ...defaultSvelteOptions, };
-        }
-        let { cssFileBaseName = name } = svelte;
-        svelte = {
-            ...defaultSvelteOptions,
-            dev: !isProduction,
-            preprocess: autoPreprocess(),
-            css(styles) {
-                // TODO: can we somehow inject this as a <link rel=""> into a html template if wanted?
-                styles.write(`${ dist }/${ cssFileBaseName }.svelte.css`);
-            },
-        };
-        options.svelte = svelte;
-    }
-    console.table(info);
-
-    // js bundle
-    const jsIn = path.resolve(__dirname, `${ src }/${ entry }`);
-    const jsOut = path.resolve(__dirname, `${ dist }/${ name }.js`);
-
-    // html template
-
-    if (options.htmlTemplate) {
-
-        const htmlIn = path.resolve(__dirname, `${ src }/${ options.htmlTemplate.template }`);
-        const htmlOut = path.resolve(__dirname, `${ dist }/${ options.htmlTemplate.page }.html`);
-        options.htmlTemplate = {
-            template: htmlIn,
-            target: htmlOut,
-        };
-    }
-
-    // devserver
-    let serveDir;
-    if (options.devServer) {
-        serveDir = path.resolve(__dirname, dist);
-    }
-
-    /**
-     * plugins
-     * */
-    const resolveDedupe = [];
-    if (options.svelte) {
-        resolveDedupe.push("svelte");
-    }
-    const plugins = [
-        resolve({
-            browser: true,
-            dedupe: resolveDedupe
-        }),
-        commonjs(),
-    ];
-
-    if (options.styles) {
-        plugins.push(stylesPlugin(options.styles));
-    }
-
-    if (options.svelte) {
-        plugins.push(sveltePlugin(options.svelte));
-    }
-    if (options.htmlTemplate) {
-        plugins.push(htmlTemplatePlugin(options.htmlTemplate));
-    }
-
-    if (!isProduction) {
-        if (options.devServer) {
-            if (options.devServer.livereload) {
-                plugins.push(liveReloadPlugin());
+        if (styles) {
+            if ("boolean" == typeof styles) {
+                styles = {};
             }
 
-            plugins.push(
-                // serve according to settings
-                serve({
-                    port: devServer.port,
-                    contentBase: serveDir,
-                    historyApiFallback: true,
-                })
-            );
+            styles = options.styles = { ...defaultStylesOptions, ...styles };
+
+            // transform the "extract" prop to this api
+            const { extract } = styles;
+            const mode = extract ?
+                [ "extract" ] :
+                [ "inject" ];
+            delete styles.extract;
+
+            // minimize as told to, OR do as top prop says
+            let { minimize } = styles;
+            if ("boolean" != typeof styles.minimize) {
+                minimize = globalMinimize;
+            }
+            styles = options.styles = {
+                ...styles,
+                mode,
+                minimize,
+            };
         }
-    }
 
-    if (globalMinimize) {
-        plugins.push(terser());
-    }
+        if (htmlTemplate) {
+            if ("boolean" == typeof htmlTemplate) {
+                htmlTemplate = {};
+            }
+            htmlTemplate = options.htmlTemplate = { ...defaultHtmlTemplateOptions, ...htmlTemplate, };
+        }
+        if (devServer) {
+            if ("boolean" == typeof devServer) {
+                devServer = {};
+            }
+            devServer = options.devServer = { ...defaultDevserverOptions, ...devServer, };
+            info["Port"] = devServer.port;
+            info["Use livereload"] = devServer.livereload;
+        }
 
-    const result = {
-        input: jsIn,
-        output: {
-            name,
-            format: "iife",
-            file: jsOut,
-            sourcemap,
-            assetFileNames: "[name][extname]",
-        },
-        watch: {
-            clearScreen: false,
-        },
-        plugins,
+        if (svelte) {
+            if ("boolean" == typeof svelte) {
+                svelte = { ...defaultSvelteOptions, };
+            }
+            let { cssFileBaseName = name } = svelte;
+            svelte = {
+                ...defaultSvelteOptions,
+                dev: !isProduction,
+                preprocess: autoPreprocess(),
+                css(styles) {
+                    // TODO: can we somehow inject this as a <link rel=""> into a html template if wanted?
+                    styles.write(`${ dist }/${ cssFileBaseName }.svelte.css`);
+                },
+            };
+            options.svelte = svelte;
+        }
+        console.table(info);
+
+        // js bundle
+        const jsIn = path.resolve(__dirname, `${ src }/${ entry }`);
+        const jsOut = path.resolve(__dirname, `${ dist }/${ name }.js`);
+
+        // html template
+
+        if (options.htmlTemplate) {
+
+            const htmlIn = path.resolve(__dirname, `${ src }/${ options.htmlTemplate.template }`);
+            const htmlOut = path.resolve(__dirname, `${ dist }/${ options.htmlTemplate.page }.html`);
+            options.htmlTemplate = {
+                template: htmlIn,
+                target: htmlOut,
+            };
+        }
+
+        // devserver
+        let serveDir;
+        if (options.devServer) {
+            serveDir = path.resolve(__dirname, dist);
+        }
+
+        /**
+         * plugins
+         * */
+        const resolveDedupe = [];
+        if (options.svelte) {
+            resolveDedupe.push("svelte");
+        }
+        const plugins = [
+            resolve({
+                browser: true,
+                dedupe: resolveDedupe
+            }),
+            commonjs(),
+        ];
+
+        if (options.styles) {
+            plugins.push(stylesPlugin(options.styles));
+        }
+
+        if (options.svelte) {
+            plugins.push(sveltePlugin(options.svelte));
+        }
+        if (options.htmlTemplate) {
+            plugins.push(htmlTemplatePlugin(options.htmlTemplate));
+        }
+
+        if (!isProduction) {
+            if (options.devServer) {
+                if (options.devServer.livereload) {
+                    plugins.push(liveReloadPlugin());
+                }
+
+                plugins.push(
+                    // serve according to settings
+                    serve({
+                        port: devServer.port,
+                        contentBase: serveDir,
+                        historyApiFallback: true,
+                    })
+                );
+            }
+        }
+
+        if (globalMinimize) {
+            plugins.push(terser());
+        }
+
+        const result = {
+            input: jsIn,
+            output: {
+                name,
+                format: "iife",
+                file: jsOut,
+                sourcemap,
+                assetFileNames: "[name][extname]",
+            },
+            watch: {
+                clearScreen: false,
+            },
+            plugins,
+        };
+
+        return result;
+
     };
-
-    return result;
 
 };
